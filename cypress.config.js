5b32ea78321ff2b48743b93f22e93807e8cbb0c3
Matheus Zichiam <matheuscz.dev@gmail.com>
2022-06-22 14:51:28 -0300

    Commit atualizado

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "gjw9sq"
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
+  }
+  ,
 });
