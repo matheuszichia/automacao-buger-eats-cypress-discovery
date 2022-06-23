2bd241faa642d75d09a15d2bed7f72acaf192366
Matheus Zichiam <matheuscz.dev@gmail.com>
2022-06-23 11:13:36 -0300

    Mudança na record key

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: "gmjcuru",
+  projectId: "mjcuru",
   e2e: {
     baseUrl: 'https://buger-eats-qa.vercel.app'
   }
